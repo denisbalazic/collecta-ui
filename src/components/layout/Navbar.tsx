5a8442a19c3461a9694fd5c91e27b8b079ddd862
denis balazic <denis.balazic@nobula.eu>
2022-02-25 21:09:22 +0100

    feat: Add few routes and pages

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {getDynamicRoute, getRoute, routeInd} from '../../router/routes';
 
 const Navbar = () => {
     return (
         <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/Register">Register</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/collections">Collections</NavLink>
+            <NavLink to={getRoute(routeInd.HOME)}>Home</NavLink>
+            <NavLink to={getRoute(routeInd.REGISTER)}>Register</NavLink>
+            <NavLink to={getRoute(routeInd.LOGIN)}>Login</NavLink>
+            <NavLink to={getRoute(routeInd.COLLECTIONS)}>Collections</NavLink>
+            <NavLink to={getDynamicRoute(routeInd.COLLECTION, ['11111', '2222'])}>Collections</NavLink>
         </div>
     );
 };
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,29 +3,59 @@ import Register from '../views/auth/Register';
 import Login from '../views/auth/Login';
 import Collections from '../views/collection/Collections';
 
+export enum routeInd {
+    HOME = 'HOME',
+    REGISTER = 'REGISTER',
+    LOGIN = 'LOGIN',
+    COLLECTIONS = 'COLLECTIONS',
+    COLLECTION = 'COLLECTION',
+}
+
 export const routes = [
     {
+        index: routeInd.HOME,
         title: 'Home',
         path: '/',
         roles: [],
         component: Home,
     },
     {
+        index: routeInd.REGISTER,
         title: 'Register',
         path: '/register',
         roles: [],
         component: Register,
     },
     {
+        index: routeInd.LOGIN,
         title: 'Login',
         path: '/login',
         roles: [],
         component: Login,
     },
     {
+        index: routeInd.COLLECTIONS,
         title: 'Collections',
         path: '/collections',
         roles: [],
         component: Collections,
     },
+    {
+        index: routeInd.COLLECTION,
+        title: 'Collections',
+        path: '/collections/:collectionId/item/:itemId',
+        roles: [],
+        component: Collections,
+    },
 ];
+
+export const getRoute = (index: routeInd) => {
+    return routes.find((route) => route.index === index)?.path || '';
+};
+
+export const getDynamicRoute = (index: routeInd, pathVariables: string[]) => {
+    const route = pathVariables.reduce((acc, curr) => {
+        return acc.replace(/:\w+(?=\/|$)/, curr);
+    }, routes.find((route) => route.index === index)?.path || '');
+    return route;
+};
