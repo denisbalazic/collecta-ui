dcf4c08cef6b7daa3bf4180e021a2073c6ba4beb
denis balazic <denis.balazic@nobula.eu>
2022-03-03 18:02:25 +0100

    dev: Implement redux devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,12 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import reducer, {reducerPreloadedState} from './reducer/reducer';
 import {authSaga} from './saga/auth/auth.saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(reducer, reducerPreloadedState, applyMiddleware(sagaMiddleware));
+const composeEnhancers = process.env.NODE_ENV === `development` ? (composeWithDevTools as typeof compose) : compose;
+
+export const store = createStore(reducer, reducerPreloadedState, composeEnhancers(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(authSaga);
