564e0e303a15e571fbd8bbd790087f3c86491474
denis balazic <denis.balazic@nobula.eu>
2022-03-05 09:35:24 +0100

    Add register and login forms

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import {useDispatch} from 'react-redux';
 
 const Login = () => {
+    // const dispatch = useDispatch();
+    const [authCredentials, setAuthCredentials] = useState({
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e: any) => {
+        setAuthCredentials({
+            ...authCredentials,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        // dispatch()
+    };
+
     return (
         <div>
             <p>Login</p>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="email"
+                        value={authCredentials.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        placeholder="password"
+                        value={authCredentials.password}
+                        onChange={handleChange}
+                    />
+                </form>
+            </div>
         </div>
     );
 };
--- src/views/auth/Register.tsx
+++ src/views/auth/Register.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Register = () => {
+    // const dispatch = useDispatch();
+    const [authCredentials, setAuthCredentials] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmedPassword: '',
+    });
+
+    const handleChange = (e: any) => {
+        setAuthCredentials({
+            ...authCredentials,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        // dispatch()
+    };
+
     return (
         <div>
             <p>Register</p>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="name"
+                        value={authCredentials.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="email"
+                        value={authCredentials.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        placeholder="password"
+                        value={authCredentials.password}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="confirmedPassword"
+                        placeholder="confirm password"
+                        value={authCredentials.confirmedPassword}
+                        onChange={handleChange}
+                    />
+                </form>
+            </div>
         </div>
     );
 };
