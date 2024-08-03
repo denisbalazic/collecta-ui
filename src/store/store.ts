eb02971c6292f9eff662403c0d2f5f405f45629a
denis balazic <denis.balazic@nobula.eu>
2024-08-03 14:29:26 +0200

    Migrate to vite
    Updated react, rect-dom, redux and related dependencies versions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import {applyMiddleware, compose, createStore} from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {composeWithDevTools} from '@redux-devtools/extension';
 import reducer, {reducerPreloadedState} from './reducer/reducer';
 import {authSaga} from './saga/auth/auth.saga';
 import {collectionSaga} from './saga/collection/collection.saga';
@@ -10,7 +10,11 @@ const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = process.env.NODE_ENV === `development` ? (composeWithDevTools as typeof compose) : compose;
 
-export const store = createStore(reducer, reducerPreloadedState, composeEnhancers(applyMiddleware(sagaMiddleware)));
+export const store = createStore(
+    reducer,
+    reducerPreloadedState as unknown as undefined,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(authSaga);
 sagaMiddleware.run(collectionSaga);
 sagaMiddleware.run(userSaga);
