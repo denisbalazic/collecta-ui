7705cb3305275cbbb68ade5f9fba6cb29320a9c9
denis balazic <denis.balazic@nobula.eu>
2024-09-18 17:50:48 +0200

    Setup cypress resetting test db

--- cypress/e2e/auth/auth.cy.js
+++ cypress/e2e/auth/auth.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Auth spec', () => {
+    before(() => {
+        cy.resetDB();
+    });
+
     beforeEach(() => {
         cy.viewport(1200, 800);
         cy.clearLocalStorage();
@@ -9,6 +13,12 @@ describe('Auth spec', () => {
     it('displays register form', () => {
         cy.visit('/register');
         cy.get('[data-testid=register-form]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=name]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=email]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=password]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=confirmedPassword]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=termsConfirmed]').should('be.visible');
+        cy.get('[data-testid=register-form] button[type=submit]').should('be.disabled');
     });
 
     it('registers a new user', () => {
@@ -21,5 +31,13 @@ describe('Auth spec', () => {
         cy.get('[data-testid=register-form] input[type=checkbox]').check();
         cy.get('[data-testid=register-form] button[type=submit]').click();
         cy.get('[data-testid=register-success]').should('be.visible');
+    });
+
+    it('disables submit on registration if all fields are not filled', () => {
+        cy.visit('/register');
+        cy.get('[data-testid=register-form]').should('be.visible');
+        cy.get('[data-testid=register-form] input[name=name]').type('Jess E. Pinkman');
+        cy.get('[data-testid=register-form] input[name=email]').type('jess@test.eu');
+        cy.get('[data-testid=register-form] button[type=submit]').should('be.disabled');
     });
 });
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -2,8 +2,10 @@ declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Cypress {
         interface Chainable {
-            login(): Chainable;
-            /* loginAdmin(): Chainable;
+            resetDB(): Chainable;
+            seedDB(seedData: any): Chainable;
+
+            /* login(): Chainable;
             getByData(selector: string): Chainable; */
         }
     }
